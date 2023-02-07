7c0c5a1cacfbcb030bff85170bc957fd75e2b99a
Labranche <slabranche@cmontmorency.qc.ca>
2023-02-07 18:30:55 -0500

    ListeUtilisateurs vidéo 41

--- src/places/components/ListeUtilisateurs.js
+++ src/places/components/ListeUtilisateurs.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./ListeUtilisateurs.css"
+import Utilisateur from "./Utilisateur";
 
 
-function ListeUtilisateurs(){
+function ListeUtilisateurs(props){
 
+    if(props.utilisateurs.length === 0){
+        return(
+            <div className="center">
+                <h2>Aucun utilisateur</h2>
+            </div>
+        )
+    }
 
+    return <ul>
+        {props.utilisateurs.map((utilisateur) =>
+        
+        (<Utilisateur key={utilisateur.id} utilisateur={utilisateur}/>
+        )  )  }
+    </ul>
 
 }
 
--- src/utilisateur/pages/Utilisateurs.js
+++ src/utilisateur/pages/Utilisateurs.js
@@ -1,9 +1,15 @@
 import React from "react";
+import ListeUtilisateurs from "../../places/components/ListeUtilisateurs";
 
 function Utilisateurs(){
-
-    return <h2>Bienvenue sur la page Utilisateurs!</h2>
-
+    const utilisateurs = [{
+        id: "u1",
+        nom: "Sylvain Labranche",
+        image : "URL",
+        places : 3
+     }
+    ];
+    return <ListeUtilisateurs utilisateurs={utilisateurs}/>
 };
 
 export default Utilisateurs;
