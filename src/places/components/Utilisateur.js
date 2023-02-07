97c95d0bdf61b6a7460c108265b612418d53cf53
Labranche <slabranche@cmontmorency.qc.ca>
2023-02-07 18:59:00 -0500

    Ajout Link

--- src/places/components/Utilisateur.js
+++ src/places/components/Utilisateur.js
@@ -1,20 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Avatar from "../../shared/Avatar";
+
 import "./Utilisateur.css";
 
-
-function Utilisateur({utilisateur}) {
+function Utilisateur({ utilisateur }) {
   return (
     <li className="user-item">
       <div className="user-item__content">
-        <div className="user-item__image">
-            <Avatar image={utilisateur.image} alt={utilisateur.nom}/>
-        </div>
-        <div className="user-item__info">
+        <Link to={`/${utilisateur.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={utilisateur.image} alt={utilisateur.nom} />
+          </div>
+
+          <div className="user-item__info">
             <h2>{utilisateur.nom}</h2>
-            <h3>{utilisateur.places} {utilisateur.places <2 ? "place" :"places"}</h3>
-            
-        </div>
+            <h3>
+              {utilisateur.places} {utilisateur.places < 2 ? "place" : "places"}
+            </h3>
+          </div>
+        </Link>
       </div>
     </li>
   );
