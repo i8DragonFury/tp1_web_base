fd7d65f63dbdb963c3251a53e76700902088619e
Labranche <slabranche@cmontmorency.qc.ca>
2023-02-07 18:19:02 -0500

    Util et ListeUtil

---
+++ src/places/components/ListeUtilisateurs.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ListeUtilisateurs.css"
+
+
+function ListeUtilisateurs(){
+
+
+
+}
+
+export default ListeUtilisateurs
---
+++ src/places/components/Utilisateur.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Utilisateur.css"
+
+
+function Utilisateur(){
+
+
+
+}
+
+export default Utilisateur
